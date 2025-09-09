cc5078e0992f6ada2a1f9a8097abd6f7f2a6ae53
anthonilm <tmcelrath26@gmail.com>
2025-09-09 11:13:29 -0500

    How about now

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -99,7 +99,7 @@ export default function AboutPage() {
             </h2>
             <h3 style={styles.heroSubtitle}>Founder & Consultant</h3>
             <p style={styles.heroDescription}>
-I help people transform anxiety into action, structure their energy into creativity and generativity, and convert reflection into lasting progress. Goals may include improving focus, building consistent habits, strengthening emotional intelligence, boosting performance, achieving a growth mindset, and sustaining optimum mental performance.             </p>
+I help people transform anxiety into action, structure their energy into creativity and generativity, and convert reflection into lasting progress. Goals may include improving focus, building consistent habits, strengthening emotional intelligence, boosting performance, achieving a growth mindset, and sustaining optimum mental performance. </p>
           </div>
         </section>
 
