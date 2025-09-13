07d59512ebace8f515402052978bbfe89d938703
anthonilm <tmcelrath26@gmail.com>
2025-09-13 17:02:32 -0500

    in health care

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -432,7 +432,7 @@ function Hero({ taupe, yvesBlue, ivory, setShowCalendly }: any) {
             textAlign: "left",
           }}
         >
-          Anthoni McElrath, M.A. & Licensed Integrative Mental Health Coach.
+          Anthoni McElrath, M.A. & License in Integrative Mental Health Care.
         </div>
 
         {/* Instagram icon */}
